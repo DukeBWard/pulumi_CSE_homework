a7def30e62c97c45b5bc3ceaec55d4b9536c0324
Luke Ward <wardlukeb@gmail.com>
2024-09-19 08:56:50 -0400

    got imge to work with interpolation

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const docker = require('@pulumi/docker');
 const config = new pulumi.Config();
 
 // the config value
-const customMessage = config.require('customMessage');
+const customMessage = config.require('MESSAGE');
 
-const resourceGroup = new azure.resources.ResourceGroup('resourceGroup');
+const resourceGroup = new azure.resources.ResourceGroup('temp_rg_group');
 
-const registry = new azure.containerregistry.Registry('registry', {
+const registry = new azure.containerregistry.Registry('tempregistry', {
     resourceGroupName: resourceGroup.name,
     sku: { name: 'Basic' },
     adminUserEnabled: true,
@@ -25,3 +25,18 @@ const credentials = pulumi.all([registry.name, resourceGroup.name]).apply(
 
 const adminUsername = credentials.apply((c) => c.username);
 const adminPassword = credentials.apply((c) => c.passwords[0].value);
+
+const imageName = 'CSE_exercise';
+const image = new docker.Image('CSE_exercise-image', {
+    imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:v1.0.0`,
+    build: {
+        context: '.',
+    },
+    registry: {
+        server: registry.loginServer,
+        username: adminUsername,
+        password: adminPassword,
+    },
+});
+
+
